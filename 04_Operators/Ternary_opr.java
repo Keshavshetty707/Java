6470a7cf5c205676786cde93ec6b24ff1e0d05c7
Keshavshetty707 <mkeshav9241217275@gmail.com>
2024-09-07 07:37:16 +0530

    Ternary operator file

---
+++ 04_Operators/Ternary_opr.java
@@ -0,0 +1,17 @@
+
+public class Ternary_opr {
+    public static void main(String[] args) {
+        //Ternary Operator : Java ternary operator is the only conditional operator that takes three operands
+
+        int n1 = 5, n2 = 10, max;
+
+        System.out.println("First num: " + n1);
+        System.out.println("Second num: " + n2);
+
+        // Largest among n1 and n2
+        max = (n1 > n2) ? n1 : n2;
+
+        // Print the largest number
+        System.out.println("Maximum is = " + max);
+    }    
+}
