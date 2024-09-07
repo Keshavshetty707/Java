7485a1fa988630cffb3014957288961649c87c81
Keshavshetty707 <mkeshav9241217275@gmail.com>
2024-09-07 07:36:09 +0530

    Assignment operator file

---
+++ 04_Operators/Assignment_opr.java
@@ -0,0 +1,79 @@
+
+public class Assignment_opr {
+    public static void main(String[] args) {
+        // =
+        int numb = 50;
+        System.out.println("\n : (Operator)");
+        System.out.println("numb : "+ numb);
+
+        //+= (Operator)
+        int numb2 = 50;
+        System.out.println("\n+= (Operator)");
+        System.out.println("value of numb2 before += operation: " + numb2);
+        numb2 += 10;
+        System.out.println("value of numb2 after =+ operation : " + numb2);
+        
+        //-= (Operator)
+        int numb3 = 50;
+        System.out.println("\n-= (Operator)");
+        System.out.println("value of numb3 before -= operation: " + numb3);
+        numb3 -= 10;
+        System.out.println("value of numb3 after -= operation : " + numb3);
+
+        // *= (Operator)
+        int numb4 = 50;
+        System.out.println("\n*= (Operator)");
+        System.out.println("value of numb4 before *= operation: " + numb4);
+        numb4 *= 10;
+        System.out.println("value of numb4 after *= operation : " + numb4);
+
+        // /= (Operator)
+        int numb5 = 50;
+        System.out.println("\n/= (Operator)");
+        System.out.println("value of numb5 before /= operation: " + numb5); 
+        numb5 /= 10;
+        System.out.println("value of numb5 after /= operation : " + numb5);
+
+        // %= (Operator)
+        int numb6 = 50;
+        System.out.println("\n%= (Operator)");
+        System.out.println("value of numb6 before %= operation: " + numb6);
+        numb6 %= 10;
+        System.out.println("value of numb6 after %= operation : " + numb6);
+
+        // &= (Bitwise AND Assignment Operator)
+        int numb7 = 50;
+        System.out.println("\n&= (Bitwise AND Assignment Operator)");
+        System.out.println("value of numb7 before &= operation: " + numb7);
+        numb7 &= 10;
+        System.out.println("value of numb7 after &= operation : " + numb7);
+
+        // |= (Bitwise OR Assignment Operator)
+        int numb8 = 50;
+        System.out.println("\n|= (Bitwise OR Assignment Operator)");
+        System.out.println("value of numb8 before |= operation: " + numb8);
+        numb8 |= 10;
+        System.out.println("value of numb8 after |= operation : " + numb8);
+
+        // ^= (Bitwise XOR Assignment Operator)
+        int numb9 = 50;
+        System.out.println("\n^= (Bitwise XOR Assignment Operator)");
+        System.out.println("value of numb9 before ^= operation: " + numb9);
+        numb9 ^= 10;
+        System.out.println("value of numb9 after ^= operation : " + numb9);
+
+        // >>= (Right Shift Assignment Operator
+        int numb10 = 50;
+        System.out.println("\n>>= (Right Shift Assignment Operator");
+        System.out.println("value of numb10 before >>= operation: " + numb10);
+        numb10 >>= 2;
+        System.out.println("value of numb10 after >>= operation : " + numb10);
+
+        // <<=  (Left Shift Assignment Operator)
+        int numb11 = 50;
+        System.out.println("\n<<= (Left Shift Assignment Operator)");
+        System.out.println("value of numb11 before <<= operation: " + numb11);
+        numb11 <<= 2;
+        System.out.println("value of numb11 after <<= operation : " + numb11);
+    }
+}
